3524c1f0452afb56efe30114fc4ceef8db5ebe3f
GuangsenWang <dev.guangsen.wang@gmail.com>
2019-11-21 21:51:42 +0800

    bug fix

--- src/main/groovy/jp/leafytree/gradle/AndroidScalaPlugin.groovy
+++ src/main/groovy/jp/leafytree/gradle/AndroidScalaPlugin.groovy
@@ -219,7 +219,7 @@ class AndroidScalaPlugin implements Plugin<Project> {
         scalaCompileTask.sourceCompatibility = javaCompileTask.sourceCompatibility
         scalaCompileTask.targetCompatibility = javaCompileTask.targetCompatibility
         scalaCompileTask.scalaCompileOptions.encoding = javaCompileTask.options.encoding
-        scalaCompileTask.classpath = javaCompileTask.classpath + project.files(androidPlugin.getAndroidBuilder(null).getBootClasspath(androidPlugin.sdkParser))
+        scalaCompileTask.classpath = javaCompileTask.classpath + project.files(androidPlugin.getAndroidBuilder(android.applicationVariants[0]).getBootClasspath(androidPlugin.sdkParser))
         scalaCompileTask.scalaClasspath = compilerConfiguration.getAsFileTree()
         scalaCompileTask.zincClasspath = zincConfiguration.getAsFileTree()
         scalaCompileTask.scalaCompileOptions.incrementalOptions.analysisFile = new File(variantWorkDir, "analysis.txt")
